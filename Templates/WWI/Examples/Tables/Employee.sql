c6e5bfe15123acd65b2f876fd5ec04dfe2d170f7
Oleg Shidrikov <oleg.shidrikov@sm-analytics.com>
2019-01-09 15:56:21 +0500

    WWI database and templates

---
+++ Templates/WWI/Examples/Tables/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Examples].[Employee] (
+    [EmployeeId]     INT      NOT NULL,
+    [EmployeeNumber] CHAR (8) NOT NULL,
+    [ManagerId]      INT      NULL,
+    CONSTRAINT [PKEmployee] PRIMARY KEY CLUSTERED ([EmployeeId] ASC),
+    CONSTRAINT [FKEmployee_Ref_ExamplesEmployee] FOREIGN KEY ([ManagerId]) REFERENCES [Examples].[Employee] ([EmployeeId])
+);
+
